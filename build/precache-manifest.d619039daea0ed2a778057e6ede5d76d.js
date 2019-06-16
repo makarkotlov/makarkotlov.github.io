2c98a9f07c8b234bc14b1368a8a844719dcb9eba
Makar Kotlov <kotlov.1996@mail.ru>
2019-06-16 19:28:07 +0600

    Build added

---
+++ build/precache-manifest.d619039daea0ed2a778057e6ede5d76d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2723234a6aced53fa4a8",
+    "url": "/static/js/main.1814ab8d.chunk.js"
+  },
+  {
+    "revision": "be3adb5e731252552ea5",
+    "url": "/static/js/2.cd2c6ca6.chunk.js"
+  },
+  {
+    "revision": "2723234a6aced53fa4a8",
+    "url": "/static/css/main.21ab71fb.chunk.css"
+  },
+  {
+    "revision": "be3adb5e731252552ea5",
+    "url": "/static/css/2.bfdbdc7a.chunk.css"
+  },
+  {
+    "revision": "9261c79e896565755dc1c0ce4535eb93",
+    "url": "/index.html"
+  }
+];
